74121ffcd5f6f77722b04de5410e1f389e8a887d
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2022-06-10 13:47:58 +0800

    update member card

--- src/components/Card/MemberCard.tsx
+++ src/components/Card/MemberCard.tsx
@@ -1,31 +1,27 @@
 import React from 'react'
-import { Box, Divider, Flex, Text, Heading } from '@chakra-ui/layout'
+import { Box, Flex, Text, Heading } from '@chakra-ui/layout'
 import { Stack, useColorModeValue, Avatar } from '@chakra-ui/react'
-type Link = {
-  link: string
-  icon: React.ReactNode
-}
-type Props = {
-  first_name?: string
-  last_name?: string
-  position?: string
-  photo?: string
-}
+import { useRouter } from 'next/router'
+import { User } from 'src/services/app/types'
 
-const MemberCard: React.FC<Props> = ({ first_name, last_name, position, photo }) => {
+const MemberCard: React.FC<{ data: User }> = ({ data }) => {
+  const router = useRouter()
+  const { first_name, last_name, position, portfolio_id, photo } = data
   return (
     <Flex justifyContent='space-between' flexDirection='column'
       rounded='lg' boxShadow='lg'
       bg={useColorModeValue('white', 'gray.700')}
       _hover={{ boxShadow: useColorModeValue('1px 1px 3px 2px #d2d2d2', '1px 1px 5px 2px #fff') }}>
       <Box flexGrow={1} p='4'>
-        <Stack spacing={2} direction='column' align="center">
-          <Avatar size='2xl' name={`${first_name} ${last_name}`} src='https://bit.ly/broken-link' />
+        <Stack spacing={2} direction='column' align="center" onClick={() => router.push(`/portfolio/${portfolio_id}`)} _hover={{ cursor: 'pointer' }}>
+          <Avatar size='2xl' name={`${first_name} ${last_name}`} src={`${process.env.NEXT_PUBLIC_GET_URL}/${photo}`} />
           <Box textAlign='center'>
             <Heading size={['md', 'lg']} fontSize='50px'>
               {first_name}
             </Heading>
-            <Text fontSize={['1xl', '2xl']}>{last_name}</Text>
+            <Heading size={['md', 'lg']} fontSize='50px'>
+              {last_name}
+            </Heading>
             <Text noOfLines={1} fontSize={['xl', '1xl']}>{position}</Text>
           </Box>
         </Stack>
