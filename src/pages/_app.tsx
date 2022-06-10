ec9c98dc0ad600f07a7ebbd0e8d5b1aba855c417
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2022-06-10 14:02:17 +0800

    add footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import theme from "../styles/theme"
-import Navigation from '../components/Navigation/Navigation'
+import { Footer, Navigation } from 'src/components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ChakraProvider theme={theme}>
     <Navigation />
     <Component {...pageProps} />
+    <Footer />
   </ChakraProvider>
 }
 
