ebdf135649079ab92435a5c781e5e204a2663c6f
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2022-06-10 14:01:58 +0800

    export components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,11 +2,15 @@ import Header from './Header/Header'
 import MemberCard from './Card/MemberCard'
 import ScrollTop from './ScrollTop/ScrollTop'
 import DynamicSkeleton from './Skeleton/Dynamic'
+import Footer from './Footer/Footer'
+import Navigation from './Navigation/Navigation'
 
 
 export {
   Header,
   MemberCard,
   ScrollTop,
-  DynamicSkeleton
+  DynamicSkeleton,
+  Navigation,
+  Footer
 }
