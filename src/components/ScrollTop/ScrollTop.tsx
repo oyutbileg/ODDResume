06c0f5ff1daaa742fbd0221b288d421cbb909004
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2022-06-10 19:50:02 +0800

    fix scroll to top button

--- src/components/ScrollTop/ScrollTop.tsx
+++ src/components/ScrollTop/ScrollTop.tsx
@@ -3,16 +3,15 @@ import React, { useEffect, useState } from 'react'
 import { useColorMode } from '@chakra-ui/color-mode'
 // Icons
 import { FaHandPointUp } from 'react-icons/fa'
-// Styled Components
-import { TopButton } from './styles'
+import { Button } from '@chakra-ui/react'
 
 const ScrollTop = () => {
   const [showScroll, setShowScroll] = useState(false)
 
   const checkScrollTop = () => {
-    if (!showScroll && window.screenY > 20) {
+    if (!showScroll && window.scrollY > 20) {
       setShowScroll(true)
-    } else if (showScroll && window.screenY <= 20) {
+    } else if (showScroll && window.scrollY <= 20) {
       setShowScroll(false)
     }
   };
@@ -31,16 +30,33 @@ const ScrollTop = () => {
 
   return (
     <>
-      <TopButton
-        showScroll={showScroll}
-        color={colorMode}
+      <Button
+        display={showScroll ? 'flex' : 'none'}
+        position="fixed"
+        bottom="50px"
+        right="50px"
+        color={colorMode === 'light' ? '#1A202C' : 'white'}
+        bg={colorMode === 'light' ? 'white' : 'rgba(255, 255, 255, 0.08)'}
         aria-label="Scroll Top Button"
         onClick={scrollTop}
         title="Go to top"
-        className=""
+        _hover={{
+          background: colorMode === 'light' ? 'rgba(255, 255, 255, 0.5)' : 'rgba(255, 255, 255, 0.05)',
+          transition: 'all 0.2s ease-in-out',
+        }}
+        outline="none"
+        alignItems="center"
+        justifyContent="center"
+        cursor="pointer"
+        py='1.5rem'
+        animation="fadeIn 0.3s"
+        transition="opacity 0.4s"
+        borderRadius="0.5rem"
+        fontSize="2rem"
+        border="none"
       >
         <FaHandPointUp />
-      </TopButton>
+      </Button>
     </>
   )
 }
