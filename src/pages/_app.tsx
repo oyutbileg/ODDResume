a4b4a760655efdfa01d5f2c49116e5273993e967
Erdenebulgan Sainkhishig <erdenebulgan.s@mobicom.mn>
2022-06-15 15:33:05 +0800

    add toaster and check login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { AuthProvider } from 'src/contexts/auth'
+import { Toaster } from 'react-hot-toast'
 import type { AppProps } from 'next/app'
-import { Footer, ScrollTop } from 'src/components'
 // Styles
 import "@fontsource/poppins/100.css"
 import "@fontsource/poppins/400.css"
@@ -8,13 +9,50 @@ import "@fontsource/poppins/700.css"
 import "animate.css"
 import '../styles/globals.css'
 import theme from "../styles/theme"
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
-    <ScrollTop />
-    <Footer />
-  </ChakraProvider>
+
+  const router = useRouter()
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (window.location.pathname !== '/' && !token) {
+      router.replace('/')
+    } else if (window.location.pathname === '/' && token) {
+      router.replace('/team')
+    }
+  }
+
+  return <>
+    <AuthProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+      gutter={8}
+      containerStyle={{
+        zIndex: 99999999,
+      }}
+      toastOptions={{
+        className: '',
+        duration: 5000,
+        style: {
+          background: '#fff',
+          color: '#000',
+        },
+        success: {
+          duration: 3000,
+          theme: {
+            primary: 'green',
+            secondary: 'black',
+          },
+        },
+      }}
+    />
+  </>
 }
 
 export default MyApp
